5b9f58702acd97d408fcb8d2a920a947b7caf927
LAPTOP-SDM8JMUV\chakit <chakirt_sony@hotmail.com>
2021-01-10 11:08:44 +0700

    Add new 3 files in varables

---
+++ variables/mutiple_word_name.py
@@ -0,0 +1,8 @@
+# Camel Casw
+myVariableName = "chakit sodsee"
+
+# Pascal Case
+MyVariableName = "chakit sodsee"
+
+# Snake Case
+my_variable_name = "chakit sodsee"
---
+++ variables/name.py
@@ -0,0 +1,12 @@
+#สามาถประกาศได้
+myvar = 'aum'
+my_var = 'aum'
+_my_var = 'aum'
+myVar = 'aum'
+MYVAR = 'aum'
+myvar2 = 'aum'
+
+#ไม่สามรถประกาศได้
+2myvaar = 'aum'
+my-var = 'aum'
+my var = 'aum'

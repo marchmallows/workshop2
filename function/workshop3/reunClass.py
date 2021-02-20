acdb11feae3803d3986d8c09a6083858f4b59fa3
LAPTOP-SDM8JMUV\chakit <chakirt_sony@hotmail.com>
2021-02-20 10:31:16 +0700

    workshop 3

---
+++ function/workshop3/reunClass.py
@@ -0,0 +1,13 @@
+import classPerson
+
+name = "chakit sodsee"
+age = 26
+address = "cha"
+
+person = classPerson.Person(name, age, address)
+
+
+print(person.get_name())
+print(person.get_age())
+print(person.get_address())
+print(person.get_info())
---
+++ function/workshop3/variables.py
@@ -0,0 +1,2 @@
+person = {"name ": "Aum ", "age": 20, "country ": "bankkok"}
+person1 = {"name ": "Aum ", "age": 20, "country ": "bankkok"}
